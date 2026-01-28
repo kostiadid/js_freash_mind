21e45a42d16592bd1895945b01174ffd0804bc2a
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-01-28 21:36:26 +0000

    regex  in js

---
+++ regex.js
@@ -0,0 +1,20 @@
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result-message");
+const checkMessageButton = document.getElementById("check-message-btn");
+
+const helpRegex = /please help/i;
+function isSpam (msg){
+  return helpRegex.test(msg)
+}
+
+function pls(){
+  if(!messageInput.value.trim()){
+    alert("Please enter a message.")
+  }
+  isSpam(messageInput.value) ? result.textContent ='Oh no! This looks like a spam message.' : result.textContent = 'This message does not seem to contain any spam.'
+
+  messageInput.value = ''
+}
+checkMessageButton.addEventListener('click',pls)
+
+
