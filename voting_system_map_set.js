6664d26279dc96a45b0bae2742e3947c3a014d44
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-01 14:53:03 -0500

    Refactor voting system functions and logic

--- voting_system_map_set.js
+++ voting_system_map_set.js
@@ -1,24 +1,48 @@
-let poll = new Map();
-let voters = new Set();
-function addOption(option){
-  if (!option){
-    return `Option cannot be empty`
+const poll = new Map();
+const voters = new Set();
+
+function addOption(option) {
+  if (option.length === 0) {
+    return `Option cannot be empty.`;
   }
-  if (!poll.has(option)){
-    poll.set(option)
-  } else{
-    return `Option "${option}" already exists.`
+  if (!poll.has(option)) {
+      poll.set(option, new Set());
+      return `Option "${option}" added to the poll.`;
   }
+  return `Option "${option}" already exists.`;
 }
 
+function vote(option, voterId) {
+  if (!poll.has(option)) {
+    return `Option "${option}" does not exist.`;
+  }
 
-function vote(option,voterId){
-  if (!poll.has(option)){
-    return `Option "${option}" does not exist.`
+  const voteKey = `${voterId}|${option}`;
+
+  if (voters.has(voteKey)) {
+    return `Voter ${voterId} has already voted for "${option}".`;
   }
-  if (voters.has(voterId)){
-    return `Voter ${voterId} has already voted for "<option>"`
-  }
-  voters.add(voterId)
-  return `Voter ${voterId} voted for "${option}"` 
+
+  voters.add(voteKey);
+  poll.get(option).add(voterId);
+
+  return `Voter ${voterId} voted for "${option}".`;
 }
+
+function displayResults() {
+  let lines = ["Poll Results:"];
+  for (let [option, votersSet] of poll.entries()) {
+    lines.push(`${option}: ${votersSet.size} votes`);
+  }
+  return lines.join("\n");
+}
+
+addOption("Turkey");
+addOption("Morocco");
+addOption("Spain");
+
+vote("Turkey", "traveler1");
+vote("Turkey", "traveler2");
+vote("Morocco", "traveler3");
+
+console.log(displayResults());
