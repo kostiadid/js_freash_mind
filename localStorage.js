c79dcd38d15a503e6d20e436fc13c6f2bef880ea
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-01 21:02:49 -0500

    Add reset function to clear task form inputs

--- localStorage.js
+++ localStorage.js
@@ -13,6 +13,14 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+  titleInput.value = '';
+  dateInput.value = '';
+  descriptionInput.value = '';
+    taskForm.classList.toggle('hidden');
+    currentTask = {};
+}
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -28,13 +36,35 @@ discardBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
 
+
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
   const taskObj = {
-    id: titleInput.value
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
+
+   if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+
+  taskData.forEach(
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+    }
+  );
+
+  taskForm.classList.toggle("hidden");
 });
