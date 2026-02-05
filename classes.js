dd0fe7cab48227b5a8292d2f98a0cf839044dfef
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-04 21:16:17 -0500

    Need to pay attentions  about  'this' and  map methods

--- classes.js
+++ classes.js
@@ -8,29 +8,45 @@ class BankAccount{
       if(depositAmount <=0){
         return "Deposit amount must be greater than zero."
       } else{
-        this.listAllDeposits(depositAmount)
+         this.transactions.push({ type: "deposit", depositAmount: depositAmount });
         this.balance = this.balance + depositAmount
         
         return `Successfully deposited $${depositAmount}. New balance: $${this.balance}`
       }
   }
-  listAllDeposits =(amount)=>{
-    this.transactions.push(amount)
+  listAllDeposits(){
+    const depositAmount = this.transactions
+      .filter(tx => tx.type === "deposit")
+      .map(tx => tx.depositAmount);
+    return "Deposits: " + depositAmount.join(",");
   }
 
-  withdraw(withAmount){
-    if(withAmount > balance|| withAmount <0){
+  withdraw(withdraw){
+    if(withdraw > this.balance|| withdraw <=0){
       return "Insufficient balance or invalid amount."
+    }else{
+      this.balance-=withdraw
+      this.transactions.push({ type: "withdraw", withdraw: withdraw });
+      return `Successfully withdrew $${withdraw}. New balance: $${this.balance}`
     }
   }
 
   checkBalance(){
-    balance = '$'+balance
-    return `Current balance: ${balance}`
+    return `Current balance: $${this.balance}`
   }
 
-  listAllWithdrawals(){
-
+  listAllWithdrawals() {
+     const withdrawals = this.transactions
+      .filter(tx => tx.type === "withdraw")
+      .map(tx => tx.withdraw); 
+    return "Withdrawals: " + withdrawals.join(",");
   }
 
 }
+const myAccount = new BankAccount();
+console.log(myAccount.deposit(100));    
+console.log(myAccount.deposit(150));    
+console.log(myAccount.withdraw(50));    
+console.log(myAccount.withdraw(30));    
+console.log(myAccount.deposit(10));    
+console.log(myAccount.deposit(21)); 
