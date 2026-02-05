1134805f4e1287f4c0398b86c9a94c24847cf70b
Kostiantyn Borysenko <132008698+kostiadid@users.noreply.github.com>
2026-02-04 19:26:07 -0500

    Update classes.js

--- classes.js
+++ classes.js
@@ -10,11 +10,20 @@ class BankAccount{
   deposit(sumDeposit){
       if(sumDeposit <=0){
         return "Deposit amount must be greater than zero."
+      } else{
+        return `Successfully deposited ${'$'+sumDeposit}. New balance: ${'$'+sumDeposit}`
       }
+  }
+
+  withdraw(withAmount){
+    if(withAmount > balance|| withAmount <0){
+      return "Insufficient balance or invalid amount."
+    }
   }
 
   checkBalance(){
 
+    return `Current balance: ${'$'+balance}`
   }
 
   listAllWithdrawals(){
